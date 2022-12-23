07b7b3b3eb0ee68725b8989950672667f490e965
Erick Monteiro <monteiro.ck@hotmail.com>
2022-12-23 16:07:15 -0300

    finish project

--- frontweb/src/components/Button/index.tsx
+++ frontweb/src/components/Button/index.tsx
@@ -3,10 +3,9 @@ import './styles.css';
 const Button = () => {
   return (
     <div className="btn-container">
-        <button className="btn btn-primary ">
-          <h6>Ver Catálogo</h6>
+        <button className="btn btn-primary">
+          <h6>ver catalogo</h6>
         </button>
-       <p>Comece agora a navegar</p>
     </div>
   );
 };
--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,22 +1,28 @@
 import './styles.css';
-import carImageHome from '../../assets/images/product-car.svg';
+import { Link } from 'react-router-dom';
+import {ReactComponent as CarHeader} from '../../assets/images/product-car.svg';
+import ButtonHome from 'components/Button/index';
 
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="base-card home-card">
+      <div className="home-card">
         <div className="home-content-container">
-          <div>
-            <h1>O carro perfeito para você</h1>
-            <p>
-              Conheça nossos carros e dê mais um passo na realização do seu
-              sonho.
-            </p>
-          </div>
+          <h1>O carro perfeito para você!</h1>
+          <p>
+          Conheça nossos carros e dê mais um passo na realização do seu sonho
+          </p>
         </div>
-
         <div className="home-image-container">
-          <img src={carImageHome} alt="Imagem do Produto Carro" />
+             < CarHeader />
+        </div>
+      </div>
+      <div className="home-button-container base-card">
+        <Link to="/products">
+          <ButtonHome />  
+        </Link>
+        <div className="button-content-container">
+          <p> Comece agora a navegar</p>
         </div>
       </div>
     </div>
